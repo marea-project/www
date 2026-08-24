8dc663d9699b6841442070cd3649460c28aa19be
Ash <ash.smith@soton.ac.uk>
2026-08-24 15:15:41 +0100

    ...

--- js.js
+++ js.js
@@ -11086,19 +11086,18 @@ $(document).ready(function()
 
 	// Create the map object with some sensible defaults
 	map = L.map('map', {
-		zoomControl: true,
 		center: [0, 0],
 		zoom: 17,
 		maxZoom: 20,
 		zoomControl: false
 	});
 	map.fitBounds(grids.getBounds());
-    L.tileLayer('https://tiles.flarpyland.com/sat/{z}/{x}/{y}.png', {
+    L.tileLayer('https://tiles.flarpyland.com/lite/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 	grids.addTo(map);
 	markers.addTo(map);
-	L.Control.Zoom({ position: 'bottomright' }).addTo(map);
+	new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
 });
 
