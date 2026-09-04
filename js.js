c965fd391d4ee76cadaf9ff3a3cbff3fbf0ff4b2
Ash <ash.smith@soton.ac.uk>
2026-09-04 14:26:27 +0100

    Feature commit, https://github.com/ads04r/marea-www-builder/commit/506eafd47c31f0404047f627b0945f2c8a3b3bd8

--- js.js
+++ js.js
@@ -11292,50 +11292,6 @@ $(document).ready(function()
 
 });
 
-function get_coast_geojson()
-{
-	var ret = {};
-	$.ajax({
-		url: 'data/coast_buffer.json',
-		async: false,
-		success: function(data) { ret = data; }
-	});
-	return ret;
-}
-
-function CoastBufferFeatureSet()
-{
-	FeatureSet.call(this);
-	this.layer = [];
-    var me = this;
-	this.init = function()
-	{
-        this.layer = L.geoJSON(get_coast_geojson(), {
-            style: function(feature) {
-                return {
-                    stroke: true, fill: true, color: '#0000FF', fillColor: '#7FAFFF', fillOpacity: 0.2, weight: 1
-                }
-            }
-        });
-	}
-	this.title = function() { return("Coastal Buffer"); }
-	this.description = function() { return("Displays a perimiter line 2km from the coast, marking MarEA's remit."); }
-	this.show = function()
-	{
-		map.addLayer(this.layer);
-	}
-	this.hide = function()
-	{
-		map.removeLayer(this.layer);
-	}
-	this.update = function()
-	{
-	}
-    this.startsVisible = function() { return false; }
-}
-CoastBufferFeatureSet.prototype = Object.create(FeatureSet.prototype);
-FSREG.addFeatureSet('coast_buffer', new CoastBufferFeatureSet());
-
 /**
  * Defines the grid squares and sites Layers
  * @constructor
@@ -11357,7 +11313,7 @@ function GridSquaresFeatureSet()
 	FeatureSet.call(this);
 	this.layer = [];
     this.markers = L.markerClusterGroup({
-        spiderfyOnMaxZoon: false,
+        spiderfyOnMaxZoom: false,
         showCoverageOnHover: false,
         zoomToBoundsOnClick: true
     });
@@ -11423,6 +11379,173 @@ function GridSquaresFeatureSet()
 GridSquaresFeatureSet.prototype = Object.create(FeatureSet.prototype);
 FSREG.addFeatureSet('grid_squares', new GridSquaresFeatureSet());
 
+function get_unfinished_grids_geojson()
+{
+	var ret = {};
+	$.ajax({
+		url: 'data/unfinished_marea_grids.json',
+		async: false,
+		success: function(data) { ret = data; }
+	});
+	return ret;
+}
+
+function UnfinishedGridSquaresFeatureSet()
+{
+	FeatureSet.call(this);
+	this.layer = [];
+    var me = this;
+	this.init = function()
+	{
+        this.layer = L.geoJSON(get_unfinished_grids_geojson(), {
+            style: function(feature) {
+                var url = 'data/' + feature.properties.name + '.json';
+                return {
+                    stroke: true, fill: true, color: '#FF0000', fillColor: '#FF7F7F', fillOpacity: 0.2, weight: 1
+                }
+            },
+            onEachFeature(feature, layer) {
+                layer.bindPopup('<h1>' + feature.properties.name + '</h1>');
+            }
+        });
+	}
+	this.title = function() { return("Incomplete Grids"); }
+	this.description = function() { return("Displays all grid squares within MarEA's remit that do not yet have any sites documented."); }
+	this.show = function()
+	{
+		map.addLayer(this.layer);
+	}
+	this.hide = function()
+	{
+		map.removeLayer(this.layer);
+	}
+	this.update = function()
+	{
+	}
+    this.startsVisible = function() { return false; }
+}
+UnfinishedGridSquaresFeatureSet.prototype = Object.create(FeatureSet.prototype);
+FSREG.addFeatureSet('unfinished_grid_squares', new UnfinishedGridSquaresFeatureSet());
+
+function get_ghost_geojson()
+{
+	var ret = {};
+	$.ajax({
+		url: 'data/ghost_points.json',
+		async: false,
+		success: function(data) { ret = data; }
+	});
+	return ret;
+}
+
+function GhostPointFeatureSet()
+{
+	FeatureSet.call(this);
+	this.layer = L.markerClusterGroup({
+        spiderfyOnMaxZoom: false,
+        showCoverageOnHover: false,
+        zoomToBoundsOnClick: true,
+        iconCreateFunction: function(cluster)
+        {                
+            var childCount = cluster.getChildCount();
+
+            var c = ' marker-cluster-';
+            if (childCount < 10) {
+                c += 'small';
+            } else if (childCount < 100) {
+                c += 'medium';
+            } else {
+                c += 'large';
+            }
+
+            return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster-error marker-cluster' + c, iconSize: new L.Point(40, 40) });
+        }
+    });
+    var me = this;
+	this.init = function()
+	{
+        this.layer.addLayer(L.geoJSON(get_ghost_geojson(), {
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 6,
+                    fillColor: '#7F0000',
+                    color: '#FF0000',
+                    weight: 1,
+                    fillOpacity: 0.9
+                });
+            },
+            style: function(feature) {
+                return {
+                    stroke: true, fill: true, color: '#FF0000', fillColor: '#FF7F7F', fillOpacity: 0.2, weight: 1
+                }
+            }, 
+            onEachFeature(feature, layer) {
+                layer.bindPopup('<h1>' + feature.properties.name + '</h1><div class="popuplinks"> <a target="_blank" class="btn btn-primary" href="' + feature.properties.url + '">View</a> <a target="_blank" class="btn btn-secondary" href="' + feature.properties.edit_url + '">Edit</a> </p>');
+            }
+        }));
+	}
+	this.title = function() { return("Ghost Points"); }
+	this.description = function() { return("Displays the items in the EAMENA database with no data attached to them."); }
+	this.show = function()
+	{
+		map.addLayer(this.layer);
+	}
+	this.hide = function()
+	{
+		map.removeLayer(this.layer);
+	}
+	this.update = function()
+	{
+	}
+    this.startsVisible = function() { return false; }
+}
+GhostPointFeatureSet.prototype = Object.create(FeatureSet.prototype);
+FSREG.addFeatureSet('ghost_points', new GhostPointFeatureSet());
+
+function get_coast_geojson()
+{
+	var ret = {};
+	$.ajax({
+		url: 'data/coast_buffer.json',
+		async: false,
+		success: function(data) { ret = data; }
+	});
+	return ret;
+}
+
+function CoastBufferFeatureSet()
+{
+	FeatureSet.call(this);
+	this.layer = [];
+    var me = this;
+	this.init = function()
+	{
+        this.layer = L.geoJSON(get_coast_geojson(), {
+            style: function(feature) {
+                return {
+                    stroke: true, fill: true, color: '#0000FF', fillColor: '#7FAFFF', fillOpacity: 0.2, weight: 1
+                }
+            }
+        });
+	}
+	this.title = function() { return("Coastal Buffer"); }
+	this.description = function() { return("Displays a perimiter line 2km from the coast, marking MarEA's remit."); }
+	this.show = function()
+	{
+		map.addLayer(this.layer);
+	}
+	this.hide = function()
+	{
+		map.removeLayer(this.layer);
+	}
+	this.update = function()
+	{
+	}
+    this.startsVisible = function() { return false; }
+}
+CoastBufferFeatureSet.prototype = Object.create(FeatureSet.prototype);
+FSREG.addFeatureSet('coast_buffer', new CoastBufferFeatureSet());
+
 /**
  * Defines the Tiles FeatureSet. This allows the user to toggle between map and aerial views
  */
@@ -11465,52 +11588,4 @@ TilesFeatureSet.prototype = Object.create(FeatureSet.prototype);
 FSREG.addFeatureSet('aerial', new TilesFeatureSet());
 
 // End Tiles FeatureSet
-
-function get_unfinished_grids_geojson()
-{
-	var ret = {};
-	$.ajax({
-		url: 'data/unfinished_marea_grids.json',
-		async: false,
-		success: function(data) { ret = data; }
-	});
-	return ret;
-}
-
-function UnfinishedGridSquaresFeatureSet()
-{
-	FeatureSet.call(this);
-	this.layer = [];
-    var me = this;
-	this.init = function()
-	{
-        this.layer = L.geoJSON(get_unfinished_grids_geojson(), {
-            style: function(feature) {
-                var url = 'data/' + feature.properties.name + '.json';
-                return {
-                    stroke: true, fill: true, color: '#000000', fillColor: '#FF7F7F', fillOpacity: 0.2, weight: 1
-                }
-            },
-            onEachFeature(feature, layer) {
-                layer.bindPopup('<h1>' + feature.properties.name + '</h1>' + feature.properties.html);
-            }
-        });
-	}
-	this.title = function() { return("Incomplete Grids"); }
-	this.description = function() { return("Displays all grid squares within MarEA's remit that do not yet have any sites documented."); }
-	this.show = function()
-	{
-		map.addLayer(this.layer);
-	}
-	this.hide = function()
-	{
-		map.removeLayer(this.layer);
-	}
-	this.update = function()
-	{
-	}
-    this.startsVisible = function() { return false; }
-}
-UnfinishedGridSquaresFeatureSet.prototype = Object.create(FeatureSet.prototype);
-FSREG.addFeatureSet('unfinished_grid_squares', new UnfinishedGridSquaresFeatureSet());
 
